e7aa5d5c1a809a6ca2c57406ee18b1c10156bca6
Victor Wang <wjp2013@gmail.com>
2020-08-31 19:10:51 +0800

    FEAT: 接受部分参数

--- zhima.go
+++ zhima.go
@@ -8,9 +8,19 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/google/go-querystring/query"
 )
 
-const URL = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=110000&city=110105&yys=0&port=1&time=2&ts=1&ys=0&cs=0&lb=1&sb=0&pb=4&mr=3&regions=110000"
+const URL = "http://http.tiqu.alicdns.com/getip3?num=1&type=2&port=1&time=2&ts=1&ys=0&cs=0&lb=1&sb=0"
+
+type Options struct {
+	Pro  int `url:"pro"`
+	City int `url:"city"`
+	YYS  int `url:"yys"`
+	MR   int `url:"mr"`
+	PB   int `url:"pb"`
+}
 
 type RespData struct {
 	IP         string `json:"ip"`
@@ -26,8 +36,8 @@ type Response struct {
 	Success bool       `json:"success"`
 }
 
-func GetIP() (result string) {
-	resp, err := send(bytes.NewBuffer(nil))
+func GetIP(opt Options) (result string, err error) {
+	resp, err := send(bytes.NewBuffer(nil), &opt)
 	if err != nil {
 		log.Println(err)
 		return
@@ -41,15 +51,14 @@ func GetIP() (result string) {
 		return
 	}
 
-	return result
+	return result, nil
 }
 
 func decoder(body io.Reader) (result string, err error) {
 	var resBody Response
 	err = json.NewDecoder(body).Decode(&resBody)
 	if err != nil {
-		log.Println(err)
-		return "", err
+		return
 	}
 
 	Data := resBody.Data[0]
@@ -59,14 +68,19 @@ func decoder(body io.Reader) (result string, err error) {
 	return result, nil
 }
 
-func send(body io.Reader) (*http.Response, error) {
-	return sendWithContext(context.Background(), body)
+func send(body io.Reader, opt *Options) (*http.Response, error) {
+	return sendWithContext(context.Background(), body, opt)
 }
 
 // Sending an HTTP request and accepting context.
-func sendWithContext(ctx context.Context, body io.Reader) (*http.Response, error) {
+func sendWithContext(ctx context.Context, body io.Reader, opt *Options) (*http.Response, error) {
+	v, _ := query.Values(opt)
+
+	// fmt.Print(v.Encode()) will output: "city=0&mr=1&pb=4&pro=0&yys=0"
+	APIEndpoint := fmt.Sprintf("%s&%s", URL, v.Encode())
+	fmt.Println(APIEndpoint)
 	// Change NewRequest to NewRequestWithContext and pass context it
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, URL, body)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, APIEndpoint, body)
 	if err != nil {
 		return nil, err
 	}
