0ebc7812896c3b24eaa459c37dec7de1e4d0f14c
Victor Wang <wjp2013@gmail.com>
2020-10-09 13:43:43 +0800

    FEAT: 请求超时默认20秒

--- utils.go
+++ utils.go
@@ -11,7 +11,7 @@ import (
 )
 
 func send(httpClient *http.Client, url string, body io.Reader, opt *Options) (*http.Response, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	return sendWithContext(ctx, httpClient, url, body, opt)
